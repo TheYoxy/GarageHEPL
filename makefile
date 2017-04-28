0faf2a6b34a38456abe2855526d0351027de1946
TheYoxy <floryansimar@gmail.com>
2017-04-28 15:09:03 +0200

    Compilation Jar Fonctionne

--- makefile
+++ makefile
@@ -4,13 +4,27 @@ APP=Test
 
 All:	$(APP)
 
-$(APP):  compilation
-	$(JAR) -cfv Appli.jar out/*/*.class
+$(APP):  Identifiable People Vehicules TestVehicules
+	$(JAR) -cvmf MANIFEST.MF Appli.jar -C out/ .
+
 compilation: 
 	@echo Debut de compilation
 	$(CC) -d out/ src/*/*.java -Xlint:unchecked
 	@echo Fin de compilation
+
+Vehicules:	src/Vehicules/*.java
+	@echo Compilation Vehicules
+	$(CC) -d out/ -cp out/ src/Vehicules/*.java -Xlint:unchecked
+TestVehicules:	src/test/*.java
+	@echo Compilation TestVehicules
+	$(CC) -d out/ -cp out/ src/test/*.java -Xlint:unchecked
+Identifiable:   src/Authenticate/*.java
+	@echo Compilation Authenticate
+	$(CC) -d out/ -cp out/ src/Authenticate/*.java -Xlint:unchecked
+People:	src/People/*.java
+	@echo Compilation People
+	$(CC) -d out/ -cp out/ src/People/*.java -Xlint:unchecked
 clean:
 	rm -rf out/
-
+	@mkdir out
 
--- src/test/TestVehicules.java
+++ src/test/TestVehicules.java
@@ -12,7 +12,7 @@ import static java.lang.System.exit;
 
 public class TestVehicules {
     public static void main(String[] args) throws MissingTradeMarkException {
-        if (args.length < 2) exit(0);
+        if (args.length < 1) exit(-1);
         Voiture[] voiture = new Voiture[]
                 {new Voiture("1-111-111", new TypeVoiture(4, 0, "Audi-TT"), new Client("AAA", "BBB", "CCC", "DDD", "EEE")),
                         new Voiture("1-111-111", new TypeVoiture(4, 0, "Audi-TT"), new Client("AAA", "BBB", "CCC", "DDD", "EEE")),
@@ -20,7 +20,7 @@ public class TestVehicules {
                         new Voiture("1-111-111", new TypeVoiture(4, 0, "Audi-TT"), new Client("AAA", "BBB", "CCC", "DDD", "EEE")),
                         new Voiture("1-111-111", new TypeVoiture(4, 0, "Audi-TT"), new Client("AAA", "BBB", "CCC", "DDD", "EEE"))};
         Vector<Voiture> vector = new Vector<>();
-        vector.addAll(Arrays.asList(voiture).subList(0, Integer.valueOf(args[1])));
+        vector.addAll(Arrays.asList(voiture).subList(0, Integer.valueOf(args[0])));
         for (Voiture temp : vector) {
             System.out.println(temp);
         }
