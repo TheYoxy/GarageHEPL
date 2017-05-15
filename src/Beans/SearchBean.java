e1c2ceb971e175ccc5b4582a4b0a9cc5bb7a8872
Nicolas <nicolasbonemme@hotmail.com>
2017-05-15 16:50:28 +0200

    gestion de la réception de l'envent sur searchBean

--- src/Beans/SearchBean.java
+++ src/Beans/SearchBean.java
@@ -2,6 +2,12 @@ package Beans;
 
 public class SearchBean implements ReceiveMessageListener
 {
+    public SearchBean()
+    {
+        ReceivingBean rb = new ReceivingBean();
+        rb.addReceiveMessageListener(this);
+        //rb.run();
+    }
 
     public void MessageDetected(ReceiveMessageEvent e)
     {
