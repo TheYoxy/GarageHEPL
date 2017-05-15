a2b769b93d9039c52583477dcebb344c4f872097
Nicolas <nicolasbonemme@hotmail.com>
2017-05-15 16:18:35 +0200

    event - listener

---
+++ src/Beans/ReceiveMessageEvent.java
@@ -0,0 +1,11 @@
+package Beans;
+
+import java.util.EventObject;
+
+public class ReceiveMessageEvent extends EventObject
+{
+    public ReceiveMessageEvent(Object source)
+    {
+        super(source);
+    }
+}
--- src/Beans/ReceivingBean.java
+++ src/Beans/ReceivingBean.java
@@ -1,4 +1,4 @@
-package Tools;
+package Beans;
 
 import network.NetworkBasicServer;
 
