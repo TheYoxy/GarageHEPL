b79acd0a477061d05ce90b7fc5a5dca46bb90fc5
Floryan Simar <floryansimar@gmail.com>
2017-05-18 15:48:48 +0200

    Création du beans de log

--- src/Beans/FichierLogBean.java
+++ src/Beans/FichierLogBean.java
@@ -1,20 +1,66 @@
 package Beans;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Bean qui ecrit et lit simplement dans un fichier (ne génere pas d'évenements)
  */
-public class FichierLogBean
+public class FichierLogBean implements ReceiveMessageListener,SearchFoundListener,InStockListener
 {
-    public FichierLogBean() {}
-
-    public void writeLog()
+    private String _filename;
+    private FileOutputStream _file;
+    public FichierLogBean(String filename) {
+        _filename = filename;
+        try {
+            _file = new FileOutputStream(_filename);
+        } catch (FileNotFoundException e) {
+            System.err.println("Impossible d'ouvrir le fichier " + _filename);
+            System.exit(-1);
+        }
+    }
+    public void finalize()
     {
-
+        try {
+            _file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void readLog()
-    {
+    @Override
+    public void MessageDetected(ReceiveMessageEvent e) {
+        try {
+            _file.write(e.getCommande().getBytes());
+            _file.write(new byte[] {Byte.parseByte("\n")});
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 
+    @Override
+    public void SearchDetected(SearchFoundEvent e) {
+        try {
+            _file.write(e.get_libelle().getBytes());
+            _file.write(new byte[] {Byte.parseByte("\n")});
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Override
+    public void InStockDetected(InStockEvent e) {
+        try {
+            _file.write(e.get_dateMois());
+            _file.write(new byte[] {Byte.parseByte("\n"),Byte.parseByte("\n")});
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public String get_filename() {
+        return _filename;
     }
 }
--- src/Beans/ReceivingBean.java
+++ src/Beans/ReceivingBean.java
@@ -10,10 +10,9 @@ public class ReceivingBean implements InStockListener {
     private Vector<ReceiveMessageListener> _receivingMessageListeners;
     private NetworkBasicServer _ser;
 
-    public ReceivingBean()
-    {
+    public ReceivingBean() {
         setCommande("");
-        _receivingMessageListeners = new Vector<ReceiveMessageListener>();
+        _receivingMessageListeners = new Vector<>();
     }
 
     @Override
@@ -22,50 +21,45 @@ public class ReceivingBean implements InStockListener {
         _ser.sendMessage(e.getEtatCommande() + ";" + e.getLibelle() + ";" + e.get_dateMois());
     }
 
-    public String getCommande() { return _commande; }
-    public void setCommande(String c){ _commande = c; }
+    public String getCommande() {
+        return _commande;
+    }
 
-    public void run(NetworkBasicServer serveur)
-    {
+    public void setCommande(String c) {
+        _commande = c;
+    }
+
+    public void run(NetworkBasicServer serveur) {
         _ser = serveur;
 
         String message;
 
-        while(true)
-        {
-            if((message = serveur.getMessage()).compareTo("RIEN") != 0)
-            {
+        while (true) {
+            if ((message = serveur.getMessage()).compareTo("RIEN") != 0) {
                 setCommande(message);
                 notifyReceiveMesageDetected();
             }
         }
     }
 
-    public void addReceiveMessageListener(ReceiveMessageListener rml)/*Ici ce sera que le search bean normalement*/
-    {
-       if(!_receivingMessageListeners.contains(rml))
-       {
-           _receivingMessageListeners.addElement(rml);
-       }
+    public void addReceiveMessageListener(ReceiveMessageListener rml)/*Ici ce sera que le search bean normalement*/ {
+        if (!_receivingMessageListeners.contains(rml)) {
+            _receivingMessageListeners.addElement(rml);
+        }
     }
-    public void removeReceiveMessageListener(ReceiveMessageListener rml)
-    {
-        if(_receivingMessageListeners.contains(rml))
-        {
+
+    public void removeReceiveMessageListener(ReceiveMessageListener rml) {
+        if (_receivingMessageListeners.contains(rml)) {
             _receivingMessageListeners.removeElement(rml);
         }
     }
 
-    protected void notifyReceiveMesageDetected()
-    {
+    protected void notifyReceiveMesageDetected() {
         ReceiveMessageEvent e = new ReceiveMessageEvent(this, _commande);
         int n = _receivingMessageListeners.size();
         //On active la méthode MessageDetected pour chaque listener
-        for(int i = 0; i<n; i++)
-        {
-            ReceiveMessageListener obj = _receivingMessageListeners.elementAt(i);
-            obj.MessageDetected(e);
+        for (int i = 0; i < n; i++) {
+            _receivingMessageListeners.elementAt(i).MessageDetected(e);
         }
     }
-
 }
